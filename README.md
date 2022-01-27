cae06106dfe629fd94268ce1197cfcfff438ba2e
Sunwoo <dlatjsdn205@LAPTOP-20LFDVNO.localdomain>
2022-01-27 17:34:34 +0900

    morgan

---
+++ README.md
@@ -0,0 +1,10 @@
+# Suntube Reloaded
+
+/ -> Home
+/login -> Login
+/Search -> Search
+/edit-user -> Edit user
+/delete-user -> Delete user
+/watch-video -> Watch Video
+/edit-video -> Edit Video
+/delete-video -> Delete Video
--- src/server.js
+++ src/server.js
@@ -1,33 +1,22 @@
 import express from "express";
-
+import logger from "morgan";
 const app = express();
 const PORT = 5000;
-
-const handleListen = () => {
-  console.log("The server open");
+const loggerMiddleware = logger("dev");
+const home = (req, res) => {
+  console.log("I will respond");
+  return res.send("Hello");
 };
-
-const loggerMiddleware = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const protectMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>You are protected</h1>");
-  }
-  next();
-};
-const handleHome = (req, res, next) => {
-  res.send("Hello this is local host 5000");
-};
-const privateServer = (req, res) => {
-  return res.send("<h1>Welcome to the private server</h1>");
+const login = (req, res) => {
+  return res.send("login");
 };
 
 app.use(loggerMiddleware);
-app.use(protectMiddleware);
-app.get("/", handleHome);
-app.get("/private", privateServer);
+app.get("/", home);
+app.get("/login", login);
 
-app.listen(PORT, handleListen);
+const handleListening = () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
