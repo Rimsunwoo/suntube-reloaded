5b173b016b967d2655fc982475db8f6ff3677d91
Sunwoo <dlatjsdn205@naver.com>
2022-02-19 16:28:10 +0900

    Conditionals, Iteration

--- src/controllers/videoCotroller.js
+++ src/controllers/videoCotroller.js
@@ -1,4 +1,7 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" }); ///global
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
+}; ///global
 export const search = (req, res) => res.send("search video"); ///global
 export const see = (req, res) => res.render("watch", { pageTitle: "see" });
 export const edit = (req, res) => res.render("edit");
