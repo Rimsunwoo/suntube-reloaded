37dec08124ae5427dff3ce31ff2817dabdabb704
Sunwoo <dlatjsdn205@naver.com>
2022-02-21 14:24:17 +0900

    mixin

--- src/controllers/videoCotroller.js
+++ src/controllers/videoCotroller.js
@@ -1,12 +1,38 @@
+const videos = [
+  {
+    title: "First video",
+    rating: 4,
+    views: 30,
+    comments: 3,
+    createdAt: "2 minutes ago",
+    id: 1,
+  },
+  {
+    title: "Second video",
+    rating: 4,
+    views: 30,
+    comments: 3,
+    createdAt: "2 minutes ago",
+    id: 2,
+  },
+  {
+    title: "Third video",
+    rating: 4,
+    views: 30,
+    comments: 3,
+    createdAt: "2 minutes ago",
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return res.render("home", { pageTitle: "Home", videos });
 }; ///global
-export const search = (req, res) => res.send("search video"); ///global
-export const see = (req, res) => res.render("watch", { pageTitle: "see" });
-export const edit = (req, res) => res.render("edit");
-export const upload = (req, res) => res.send("upload video");
-export const deleteVideo = (req, res) => {
-  console.log(req.param);
-  return res.send("delete video");
+export const see = (req, res) => {
+  const { id } = req.params;
+  console.log("show video", id);
+  return res.render("watch", { pageTitle: "see" });
 };
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("search video"); ///global
+export const upload = (req, res) => res.send("upload video");
+export const deleteVideo = (req, res) => res.send("delete video");
