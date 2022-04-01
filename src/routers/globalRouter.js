7585f8ec880ab83eb9b658d521584fa79e10139d
Sunwoo <dlatjsdn205@naver.com>
2022-04-01 19:37:13 +0900

    video, search

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { home, search } from "../controllers/videoCotroller";
+import { home, searchVideo } from "../controllers/videoCotroller";
 const globalRouter = express.Router();
 
 const handleHome = (req, res) => res.send("Homes");
@@ -8,5 +8,6 @@ const handleHome = (req, res) => res.send("Homes");
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", searchVideo);
 
 export default globalRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,12 +5,13 @@ import {
   postUpload,
   getEdit,
   postEdit,
+  deleteVideo,
 } from "../controllers/videoCotroller";
 
 const videoRouter = express.Router();
-
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.get("/:id", watch);
+videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
